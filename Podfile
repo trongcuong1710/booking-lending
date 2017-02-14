3efc955a3a8a04c1548e98a8926f5702d0c88338
Cuong Doan <trongcuong1710@gmail.com>
2017-02-14 17:47:47 +0700

    Changed Google pod to Google/SignIn pod

--- Podfile
+++ Podfile
@@ -5,7 +5,7 @@ target 'booklending' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
 
-  pod 'Google'
+  pod 'Google/SignIn'
 
   target 'booklendingTests' do
     inherit! :search_paths
---
+++ booklending/ViewControllers/RootController.swift
@@ -0,0 +1,24 @@
+//
+//  RootController.swift
+//  booklending
+//
+
+import UIKit
+
+class RootController: UINavigationController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
+
+extension RootController {
+    private func configGoogleSignIn() {
+        
+    }
+}
+
+//extension RootController: GIDSignInDelegate {
+//    
+//}
